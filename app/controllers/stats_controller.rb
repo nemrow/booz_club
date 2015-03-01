6867b93d8b99fb3dd7cb8bd2a45ecce6692e7fbb
Nemrow <nemrowj@gmail.com>
2015-02-28 21:42:33 -0800

    Update stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,10 @@
 class StatsController < ApplicationController
   def index
-    @searches = Search.includes(:places, :user, :search_places)
+    @searches = Search.includes(:places, :user, :search_places).order("created_at DESC")
+
+    @success_number = 0
+    @searches.each do |search|
+      @success_number += 1 if SearchPlace.where(search: search, result: SearchPlace::IN_STOCK).count > 0
+    end
   end
 end

fdff2f284d844f204ce2874094bf2bc235d8396d
Nemrow <nemrowj@gmail.com>
2015-03-07 12:01:35 -0800

    rescue bad calls from twilio

--- app/models/concerns/bulk_caller.rb
+++ app/models/concerns/bulk_caller.rb
@@ -14,12 +14,15 @@ class BulkCaller
         status_callback: ENV["BASE_URL"] + "/status_callback?place_id=#{place.id}&search_id=#{@search.id}",
         url: ENV["BASE_URL"] + "/init_call?place_id=#{place.id}&search_id=#{@search.id}"
       }
-
       search_place = SearchPlace.find_by(search: @search, place: place)
-      search_place.update(steps_completed: SearchPlace::CALL_PLACED)
-
-      @twilio = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH_TOKEN']
-      @twilio.account.calls.create data
+      begin
+        @twilio = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH_TOKEN']
+        @twilio.account.calls.create data
+        search_place.update(steps_completed: SearchPlace::CALL_PLACED)
+      rescue Exception => e
+        search_place.destroy
+        puts "ERROR TRYING TO CALL #{place.name} with id #{place.id} for search id #{@search.id}. #{e}"
+      end
     end
   end
 end
